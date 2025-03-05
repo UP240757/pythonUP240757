a0ab481e45609311bad08822e76005df05c6c967
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-05 10:32:26 -0600

    empezo dia 4

--- Ejercicios dia 4.py
+++ Ejercicios dia 4.py
@@ -1,3 +1,27 @@
 #Ejercicio 1.
 #Concatenate the string 'Thirty', 'Days', 'Of', 
 # 'Python' to a single string, 'Thirty Days Of Python'.
+palabra = 'Thirty '
+palabrados = 'Days '
+palabratres = 'of '
+palabracuatro = 'Python '
+print(palabra,)
+print(palabrados,)
+print(palabratres,)
+print(palabracuatro,)
+secuencia = palabra + "" "" + palabrados + "" "" + palabratres + "" "" + palabracuatro
+print(secuencia)
+
+#Ejercicio 2
+#Concatenate the string 'Coding', 'For' , 'All' 
+# to a single string, 'Coding For All'.
+palabra = 'Coding, For, All'
+print(palabra)
+palabrauno = 'Coding '
+palabrados= 'For '
+palabratres = 'All'
+secuencia = palabrauno + "" "" + palabrados + "" "" + palabratres
+print(secuencia)
+
+
+
