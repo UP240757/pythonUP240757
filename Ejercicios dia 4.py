39d3e97237284c374beae93e51ea3da815962a7d
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-06 12:44:09 -0600

    avance

--- Ejercicios dia 4.py
+++ Ejercicios dia 4.py
@@ -38,12 +38,41 @@ print(company)
 
 #Ejercicio 5 
 #Print the length of the company string using len() method and print().
-
+print("Ejercicio 5: ")
 longitud = len(company)
 print ("la longitud de la cadena 'company´ es :" , longitud)
 
 #Ejercicio 6
 #Change all the characters to uppercase letters using upper() method.
+print("Ejercicio 6:")
+company = 'coding for all'
+company_upper = company.upper()
+print("Los caracteres en uppercase son:", company_upper)
+
+#Ejercicio 7
+#Change all the characters to lowercase letters using lower() method.
+print ("Ejercicio 7:" )
+company = 'CODING FOR ALL'
+company_lower = company.lower ()
+print ("Los caracteres en lowercase son:", company_lower)
+
+#Ejercicio 8
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print("Ejercicio 8: ")
+company= 'Coding For All'
+company_capitalized = company.capitalize()
+company_titled = company.title()
+company_swapcased = company.swapcase()
+print('original:', company)
+print('capitalize:', company_capitalized)
+print('title:', company_titled)
+print('swapcase:', company_swapcased)
+
+#Ejercicio 9 
+#Cut(slice) out the first word of Coding For All string.
+
+
+
 
 
 
--- ejecutar.py
+++ ejecutar.py
@@ -1,9 +1,28 @@
+#Ejercicio 6
+#Change all the characters to uppercase letters using upper() method.
+print("Ejercicio 5:")
+company = 'coding for all'
+company_upper = company.upper()
+print("Los caracteres en uppercase son:", company_upper)
 
-company = 'Coding for all'
-longitud = len(company)
-print ("la longitud de la cadena 'company' es :" , longitud )
+#Ejercicio 7
+#Change all the characters to lowercase letters using lower() method.
+print ("Ejercicio 7:" )
+company = 'CODING FOR ALL'
+company_lower = company.lower()
+print ("Los caracteres en lowercase son:", company_lower)
 
-
+#Ejercicio 8
+#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print("Ejercicio 8: ")
+company= 'CODING FOR ALL'
+company_capitalized = company.capitalize()
+company_titled = company.title()
+company_swapcased = company.swapcase()
+print('original:', company)
+print('capitalize:', company_capitalized)
+print('title:', company_titled)
+print('swapcase:', company_swapcased)
 
 
 
