68f814d3f9c2319506b16d4a5aa44f6fd3bfb20c
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-06 20:38:49 -0600

    avances

--- Ejercicios dia 4.py
+++ Ejercicios dia 4.py
@@ -70,6 +70,16 @@ print('swapcase:', company_swapcased)
 
 #Ejercicio 9 
 #Cut(slice) out the first word of Coding For All string.
+print ("Ejercicio 9:")
+company = "Coding For All"
+palabras = company.split()
+newcompany = ' '.join(palabras[1:])
+print ( "El resultado de cut (slice) es:", newcompany)
+
+#Ejercicio 10
+#Check if Coding For All string contains a word Coding using the method index, find or other methods.
+print ("Ejercicio 10: ")
+
 
 
 
--- ejecutar.py
+++ ejecutar.py
@@ -1,28 +1,11 @@
-#Ejercicio 6
-#Change all the characters to uppercase letters using upper() method.
-print("Ejercicio 5:")
-company = 'coding for all'
-company_upper = company.upper()
-print("Los caracteres en uppercase son:", company_upper)
 
-#Ejercicio 7
-#Change all the characters to lowercase letters using lower() method.
-print ("Ejercicio 7:" )
-company = 'CODING FOR ALL'
-company_lower = company.lower()
-print ("Los caracteres en lowercase son:", company_lower)
-
-#Ejercicio 8
-#Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
-print("Ejercicio 8: ")
-company= 'CODING FOR ALL'
-company_capitalized = company.capitalize()
-company_titled = company.title()
-company_swapcased = company.swapcase()
-print('original:', company)
-print('capitalize:', company_capitalized)
-print('title:', company_titled)
-print('swapcase:', company_swapcased)
+#Ejercicio 9 
+#Cut(slice) out the first word of Coding For All string.
+print ("Ejercicio 9:")
+company = "Coding For All"
+palabras = company.split()
+newcompany = ' '.join(palabras[1:])
+print ( "El resultado de cut (slice) es:", newcompany)
 
 
 
