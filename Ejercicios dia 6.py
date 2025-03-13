5521f04642aacd5adf86fdc8c963c2b57e486ed2
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-13 11:45:40 -0600

    seguimiento

--- Ejercicios dia 6.py
+++ Ejercicios dia 6.py
@@ -11,8 +11,8 @@ empty_tutle = tuple()
 #Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
 brothers= ("Junior, Felipe, Daniel,")
 sisters = ("Veronica")
-print(brothers)
-print(sisters)
+print("Los hermanos son:", brothers)
+print("Las hermanas son:", sisters)
 
 
 #Ejercicio 3
@@ -31,11 +31,63 @@ print(len(["Junior","Felipe","Daniel","Veronica",]))
 
 #Ejercicio 5
 #Modify the siblings tuple and add the name of your father and mother and assign it to family_members.
-hermanos = ("Junior, Felipe, Daniel, Veronica,")
-padres = ( "Mama Evelia, Papa Hector")
-family_members = hermanos + padres 
+hermanos = "Junior, Felipe, Daniel, Veronica,"
+padres =  "Mama Evelia, Papa Hector"
+family_members = hermanos +  padres 
 print( "Mi familia completa es: ", family_members)
 
+#Exercises: Level 2
+#Ejercicio 1
+#Unpack siblings and parents from family_members
+family_members ='Junior', 'Felipe', 'Daniel', 'Veronica', 'Mama Evelia', 'Papa Hector'
+print("Los hermanos son:",family_members[:4])
+print("Los padres son:", family_members[4:])
 
 
+#Ejercicio 2
+#Create fruits, vegetables and animal products tuples. 
+#Join the three tuples and assign it to a variable called food_stuff_tp.
+frutas =  "Fresa", "Sandia", "Manzana", "Melon","Naranja" 
+vegetales = "Lechuga", "Limon", "Cebolla", "Brocoli","Zanahoria"  
+productos_animal = "Leche", "Huevos","Queso"
+food_stuff_tp = frutas + vegetales + productos_animal
+print( "la variable food_stuff_tp:", food_stuff_tp)
+
+
+
+
+
+#Ejercicio 3
+#Change the about food_stuff_tp tuple to a food_stuff_lt list
+food_stuff_tp = "Fresa", "Sandia", "Manzana", "Melon","Naranja","Lechuga", "Limon", "Cebolla", "Brocoli","Zanahoria", "Leche", "Huevos","Queso"
+food_stuff_lt = list(food_stuff_tp)
+print( "La food_stuff_lt es:",  food_stuff_lt)
+
+
+
+#Slice out the middle item or items from the food_stuff_tp tuple or food_stuff_lt list.
+
+
+
+#Slice out the first three items and the last three items from food_staff_lt list
+
+
+#Delete the food_staff_tp tuple completely
+
+
+#Check if an item exists in tuple:
+
+
+#Check if 'Estonia' is a nordic country
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
+if 'Estonia' in  nordic_countries:
+    print('Estonia esta en:', nordic_countries('Estonia'))
+else: 
+    print('No esta')
+    
+
+
+
+#Check if 'Iceland' is a nordic country
+nordic_countries = ('Denmark', 'Finland','Iceland', 'Norway', 'Sweden')
 
--- ejecutar.py
+++ ejecutar.py
@@ -1,22 +1,7 @@
-#then insert Python and SQL after Redux.
-front_end = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-back_end = ['Node','Express', 'MongoDB']
-full = front_end + back_end
-insert_index = full.index('Redux') + 1
-full[insert_index:insert_index] = ['Python', 'SQL']
-print(full)
-
-#Ejercicio 28
-#The following is a list of 10 students ages:
-print("Ejercicio 28:")
-ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-ages.insert()
-ages.insert(ages)
-print(ages[0], "," ,ages[len(ages)-1])
-print(p.countries.index('Mexico'))
-
-
-unpack = ('China', 'Russia', 'USA')
-scandic_countries =('Finland', 'Sweden', 'Norway', 'Denmark')
-print("Los primeros 3:", unpack)
-print("Los scandic countries son: ", scandic_countries)
+#Create fruits, vegetables and animal products tuples. 
+#Join the three tuples and assign it to a variable called food_stuff_tp.
+frutas =  "Fresa", "Sandia", "Manzana", "Melon","Naranja" 
+vegetales = "Lechuga", "Limon", "Cebolla", "Brocoli","Zanahoria"  
+productos_animal = "Leche", "Huevos","Queso"
+food_stuff_tp = frutas + vegetales + productos_animal
+print(food_stuff_tp)
