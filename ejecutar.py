405bba0f51d41553731a33c2ef19a1308d30d2e4
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-19 10:38:47 -0600

    avance

--- ejecutar.py
+++ ejecutar.py
@@ -1,1 +1,13 @@
+import paises as p
 
+paises = p.paises
+acum = 0
+
+for pais in paises :
+    acum = pais['population'] = acum 
+    print(pais)
+    print(type(pais))
+    print(pais['name'])
+
+    print(pais['population'])
+print("somos:", acum)
