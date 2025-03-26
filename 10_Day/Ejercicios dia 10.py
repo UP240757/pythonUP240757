e96d41717801ad3c063b0aa0d7c11227389bbf74
UP240757 <up240757@alumnos.upa.edu.mx>
2025-03-26 10:38:17 -0600

    avance

--- 10_Day/Ejercicios dia 10.py
+++ 10_Day/Ejercicios dia 10.py
@@ -1,4 +1,21 @@
+print("Ejercicios de nivel 1:")
+#Ejercicios Nivel 1:
+#Ejercicio 1:
+#Iterate 0 to 10 using for loop, do the same using while loop.
+print("Ejerciico 1:")
+#para
+for i in range(11):
+    print(i)
 
+
+#Ejercicio 2
+#Iterate 10 to 0 using for loop, do the same using while loop.
+print("Ejercicio 2:")
+i = 0
+while i < 11:
+    print(i)
+    i += 1
+#Write a loop that makes seven calls to print(), so we get on the output the following triangle:
 #         
 ##        
 ###
@@ -7,10 +24,8 @@
 ######
 #######
 
-
 for i in range(8):
     print("#"*i)
-    
 
 
 
@@ -18,12 +33,5 @@ for i in range(8):
 
 
 
-#para
-for i in range(11):
-    print(i)
 
-i = 0
-while i < 11:
-    print(i)
-    i += 1
 
