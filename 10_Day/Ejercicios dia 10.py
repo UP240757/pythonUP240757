843161df9a3cdfd8b8dbbf5fbcad2afc9671709d
UP240757 <up240757@alumnos.upa.edu.mx>
2025-04-02 10:39:53 -0600

    Terminado dia 10.

--- 10_Day/Ejercicios dia 10.py
+++ 10_Day/Ejercicios dia 10.py
@@ -175,38 +175,27 @@ print("Ejercicios de Nivel 3:")
 
 
 #Ejercicio 1
-#Go to the data folder and use the countries.py file. 
-#Loop through the countries and extract all the countries containing the word land.
+# Go to the data folder and use the countries.py file.
+# Loop through the countries and extract all the countries containing the word 'land'.
 print("Ejercicio 1:")
 import countries as p
 paises = p.countries
+paises_con_land = []
 for pais in paises:
-    if 'land' in pais['name']:
-        print(pais['name'])
+    if 'land' in pais:
+        paises_con_land.append(pais)
+print("Los paises que terminan con Land son:", paises_con_land)
+
+
+
+
+
+
+
+
+
+
         
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -225,9 +214,16 @@ for i in range(len(frutas)-1, -1, -1):
 
 
 #Ejercicio 3
-#Go to the data folder and use the countries_data.py file.
-import countries as p
-paises = p.countries
+# Go to the data folder and use the countries_data.py file.
+# 3.1.What are the total number of languages in the data
+print("Ejercicio 3 y 3.1:")
+import countries_data as cd
+number_of_lenguaje = cd.paises
+lenguajes = set()
+for pais in number_of_lenguaje:
+    for idioma in pais['languages']:
+        lenguajes.add(idioma)
+print("El total de idiomas es:", len(lenguajes))
 
 
 
@@ -240,7 +236,66 @@ paises = p.countries
 
 
 
-#What are the total number of languages in the data
+
+
+
+
+#Ejercicio 3.2
+#Find the ten most spoken languages from the data
+print("Ejercicio 3.2:")
+setLanguages = set()
+dictLanguages = {}
+
+for country in cd.paises:
+    for language in country['languages']:
+        setLanguages.add(language)
+
+for language in setLanguages:
+    dictLanguages[language] = 0
+
+for language in dictLanguages:
+    for country in cd.paises:  
+        if language in country['languages']:
+            dictLanguages[language] += country['population']
+
+sortValuesLanguagesPopulation = sorted(dictLanguages.values(), reverse=True)
+sorfKeysLanguagesPopulation = sorted(dictLanguages, key=dictLanguages.get, reverse=True)
+
+print('Los 10 idiomas más hablados en el mundo son:')
+for i in range(10):
+    print(" El Idioma es:",  sorfKeysLanguagesPopulation[i])
+    print("Cantidad de personas que lo hablan:", sortValuesLanguagesPopulation[i])
+
+
+
+
+
+
+#Ejercicio 3.3
+#Find the 10 most populated countries in the world
+print("Ejercicio 3.3:")
+paises = cd.paises
+paises_poblacion = {}
+for pais in paises:
+    paises_poblacion[pais['name']] = pais['population']
+paises_poblacion_ordenados = sorted(paises_poblacion.items(), key=lambda x: x[1], reverse=True)
+print("Los 10 países más poblados del mundo son:")
+for i in range(10):
+    print(paises_poblacion_ordenados[i][0], ":", paises_poblacion_ordenados[i][1])
+
+
+    
+
+
+        
+
+
+         
+ 
+ 
+
+
+
 
 
 
