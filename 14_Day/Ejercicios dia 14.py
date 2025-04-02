c6405057cea6e9ef4c3249f774ff6f2604a79e10
UP240757 <up240757@alumnos.upa.edu.mx>
2025-04-01 23:23:01 -0600

    avance

---
+++ 14_Day/Ejercicios dia 14.py
@@ -0,0 +1,5 @@
+print("Ejercicios de Nivel 1:")
+#Ejerciciios de nivel 1:
+#Explain the difference between map, filter, and reduce.
+print("Ejercicio 1:")
+print("La diferencia entre map, filter y reduce es que:")
