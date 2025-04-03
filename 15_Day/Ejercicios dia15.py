1d24807ed47f3f96eeebbcb10198d39de25bde17
UP240757 <up240757@alumnos.upa.edu.mx>
2025-04-03 17:39:50 -0600

    Terminado :D

--- 15_Day/Ejercicios dia15.py
+++ 15_Day/Ejercicios dia15.py
@@ -1,2 +1,88 @@
-#Ejercicio 1
+#Ejercicio 1.
 print("Ejercicio 1:")
+print("Todo esto marca error pero asi es :D")
+print("Ejemplos de errores:")
+
+
+#Ejemplo 1:
+ #Syntaxerror
+print('Hola Anaya'
+      #SyntaxError: No se ejecuta el print porque no se cierra el parentesis.
+
+#Ejemplo 2:
+#NameError     
+ print(ya me cansee)
+    #NameError: No se ejecuta el print porque no se ha definido la variable ya me cansee.
+
+#Ejemplo 3:
+ #IndexError
+ num = [100,2,30,4,50000]
+ print(num[0])
+ #IndexError: No se ejecuta el print porque no existe el indice 8 en la lista num.
+
+
+
+#Ejemplo 4:
+ #ModuleNotFoundError
+ import potaxie
+    #ModuleNotFoundError: No se ejecuta el import porque no existe el modulo potaxie.
+
+
+#Ejemplo 5:
+ #AttributeError
+ import range
+ range.ends
+    #AttributeError: No se ejecuta el import porque no existe el modulo range.
+
+
+
+#Ejemplo 6:
+ #KeyError
+ info = {'amigas':'Loren','UPA':'2025'}
+ print(info[nom])
+    #KeyError: No se ejecuta el print porque no existe la llave nom en el diccionario info.
+
+
+
+
+
+#Ejemplo 7:
+ #TypeError
+ print('soy' + 7)
+    #TypeError: No se ejecuta el print porque no se puede sumar un string con un int.
+
+
+
+
+
+#Ejemplo 8:
+ #ValueError
+ print int('Tengo hambreee')
+    #ValueError: No se ejecuta el print porque no se puede convertir un string a un int.
+
+
+
+
+
+
+#Ejemplo 9:
+ #ImportError
+ import math 2
+ import math 2
+    #ImportError: No se ejecuta el import porque no existe el modulo math 2.
+
+
+
+
+                         
+
+
+
+      
+
+
+
+
+
+
+
